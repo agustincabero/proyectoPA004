df5f71c4eabf8af9ae279e79082478ccfc17bd3c
Agustín Cabero <agustincabero@gmail.com>
2018-08-12 18:14:03 -0300

    Se finaliza proyecto con todas las funcionalidades requeridas. Codigo comentado. Extras.

--- js/pixel-art.js
+++ js/pixel-art.js
@@ -1,3 +1,4 @@
+//Definicion de variables globales.
 var palette = document.getElementById('paleta');
 var pixelGrid = document.getElementById('grilla-pixeles');
 var statusMouse = false;
@@ -37,6 +38,7 @@ colorPersonalizado.addEventListener('change',
   })
 );
 
+//Crea la paleta de colores.
 function colorPaletteMaker(){
   for (var i = 0; i < nombreColores.length; i++) {
     var newDiv = document.createElement('div');
@@ -46,6 +48,7 @@ function colorPaletteMaker(){
   }
 }
 
+//Crea la grilla de pixeles.
 function pixelsGridMaker(){
   for (var i = 0; i < 1750; i++) {
     var newPix = document.createElement('div');
@@ -53,6 +56,7 @@ function pixelsGridMaker(){
   }
 }
 
+//Setea el selector de color dependiendo del color que seleccionemos en la paleta.
 function colorSelect(){
   for (var i = 0; i < nombreColores.length; i++) {
     var colors = document.getElementsByClassName('color-paleta');
@@ -62,6 +66,7 @@ function colorSelect(){
   }
 }
 
+//Pinta el pixel segun corresponda. La primera funcion responde de acuerdo a la accion que se realice sobre la grilla (click o mouseover). La segunda se crea para no repetir código. Es la encargada de cambiar el color del pixel.
 function paintPixel(){
   pixelGrid.addEventListener('click', function(e){
     paint(e);
@@ -72,18 +77,56 @@ function paintPixel(){
     }  
   })  
 }
-
 function paint(e) {
   e.target.style.backgroundColor = document.getElementById('indicador-de-color').style.backgroundColor;
 }
 
+//Cambia el valor de la variable "statusMouse" dependiendo si el boton esta presionado o no.
 function mouseStatus(){    
   pixelGrid.addEventListener('mousedown', function() {
     statusMouse = true;
-  })
+  });
   document.addEventListener('mouseup', function() {
     statusMouse = false;
+  });
+}
+
+ //Al presionar el boton "Borrar Todo" cambia el backgroundColor de todos los pixeles a su valor original ("white") en una animación de 1 segundo.
+$(document).ready(function() {
+  var $grid = $('#grilla-pixeles').children('div');
+  var $delete = $('#borrar');
+  $delete.click(function(){
+    $grid.animate({'background-color':'white'}, 1000);     
+    $('body').css('background-image','url()');
+  });
+});
+
+//Toma el id del elemento clickeado, y lo convierte en variable y lo pasa como parametro a la funcion cargarSuperheroe(). Tambien modifica la imagen de fondo, segun corresponda.
+$(document).ready(function() {
+  var $superHeroes = $('.imgs').find('img');
+  $superHeroes.click(function(e){
+    cargarSuperheroe(eval(e.target.id));    
+    $('body').css('background-image','url(img/'+ e.target.id +'.jpg)');
   })
+});
+
+//Al presionar el botón "Guardar" ejecuta la función guardarPixelArt().
+$(document).ready(function() {
+  $('#guardar').click(guardarPixelArt);
+});
+
+//Despues de agregar un imput para seleccionar un color personalizado, setea ese color como color de fondo predeterminado en la grilla.
+function changeBGColor(){
+  var fondoPersonalizado = document.getElementById('fondo-personalizado');
+  fondoPersonalizado.addEventListener('change', 
+    (function() {
+      var bgColor = fondoPersonalizado.value;
+      var pixeles = pixelGrid.getElementsByTagName('div');
+      for (let i = 0; i < pixeles.length; i++) {
+        pixeles[i].style.backgroundColor = bgColor;        
+      }    
+    })
+  );
 }
 
 colorPaletteMaker();
@@ -91,3 +134,4 @@ pixelsGridMaker();
 colorSelect();
 paintPixel();
 mouseStatus();
+changeBGColor()
